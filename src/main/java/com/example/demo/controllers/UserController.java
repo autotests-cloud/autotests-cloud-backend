971f9e24895af4d2198d16c8d9befeb83ece6785
svasenkov <aanher@gmail.com>
2021-03-21 20:32:01 +0300

    added CRUD user and tests

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,19 +1,44 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("users")
 public class UserController {
 
-    @RequestMapping(value = "/getUsers", method = RequestMethod.GET)
-    public List<User> users() {
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers() {
 
-
-        return List.of(new User(), new User());
+        return new ResponseEntity<>(List.of(new User(), new User()), HttpStatus.OK);
     }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{login}")
+    public ResponseEntity<User> getUserById(@PathVariable String login) {
+        // return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new User(), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{login}")
+    public ResponseEntity deleteUser(@PathVariable String login) {
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
--- src/test/java/com/example/demo/UserTests.java
+++ src/test/java/com/example/demo/UserTests.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.models.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -15,13 +20,70 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UserTests {
 	@Autowired
 	protected MockMvc mvc;
+	private User user;
+	ObjectMapper mapper = new ObjectMapper();
+
+	@BeforeEach
+	void setup() {
+		user = new User("test", "testov");
+	}
 
 	@Test
 	void testGetUsersSucceeded() throws Exception {
 		mvc.perform(
 				MockMvcRequestBuilders
-						.get("/getUsers"))
+						.get("/users"))
 				.andExpect(status().isOk());
+	}
+
+	@Test
+	void testCreateUserSucceeded() throws Exception {
+		String jsonInString = mapper.writeValueAsString(user);
+
+		mvc.perform(
+				MockMvcRequestBuilders
+						.post("/users")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonInString))
+				.andExpect(status().isCreated());
+	}
+
+	@Test
+	void testGetUserByIdSucceeded() throws Exception {
+		mvc.perform(
+				MockMvcRequestBuilders
+						.get("/users/iterkin"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void testGetUserByIdNotFound() throws Exception {
+		mvc.perform(
+				MockMvcRequestBuilders
+						.get("/users/null"))
+				.andExpect(status().isNotFound());
+	}
+
+	@Test
+	void testUpdateUserSucceeded() throws Exception {
+		String jsonInString = mapper.writeValueAsString(user);
+
+		mvc.perform(
+				MockMvcRequestBuilders
+						.put("/users")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonInString))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void testDeleteUserSucceeded() throws Exception {
+
+		mvc.perform(
+				MockMvcRequestBuilders
+						.delete("/users/iterkin")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNoContent());
 	}
 
 }
