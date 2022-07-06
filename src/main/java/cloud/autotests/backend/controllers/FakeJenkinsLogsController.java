8beccfd1e4ae696a863ace6e327e90abe16fdb62
svasenkov <aanher@gmail.com>
2022-07-06 19:28:47 +0300

    add jenkins fake logs

---
+++ src/main/java/cloud/autotests/backend/controllers/FakeJenkinsLogsController.java
@@ -0,0 +1,22 @@
+package cloud.autotests.backend.controllers;
+
+import kong.unirest.Unirest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.String.format;
+
+@RestController
+@RequestMapping("jenkins")
+public class FakeJenkinsLogsController {
+
+    @GetMapping("/{id}")
+    public String getLogsById(@PathVariable String id) {
+        String jenkinsLog = Unirest
+                .get(format("https://jenkins.autotests.cloud/job/%s/2/logText/progressiveText?start=0", id))
+                .asString().getBody();
+        return jenkinsLog;
+    }
+}
