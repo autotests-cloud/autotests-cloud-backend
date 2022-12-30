73323f24796827f8e4a12050a1c4680ee474aba5
mailigors <igorsoloviov77@gmail.com>
2022-12-30 08:13:59 +0300

    set AllowedOrigins

--- src/main/java/cloud/autotests/backend/security/WebSocketConfig.java
+++ src/main/java/cloud/autotests/backend/security/WebSocketConfig.java
@@ -29,8 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
         registry.addEndpoint("/ws")
-//                .setAllowedOrigins(frontendUrl)
-//                .setAllowedOrigins("*")
+                .setAllowedOrigins(frontendUrl)
                 .withSockJS();
     }
 
